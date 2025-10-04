b66d2b8ac966afd6525783cec9146b1434e76eca
Jason Schmitt <schmitty890@gmail.com>
2025-10-04 00:15:35 -0400

    test click

--- index.js
+++ index.js
@@ -4,10 +4,13 @@ function main() {
   console.log('starting');
   sleep(2000);
 
-  robot.moveMouseSmooth(30, 100);
-  //   robot.mouseClick();
-  sleep(2000);
+  robot.moveMouseSmooth(331, 707);
+  robot.mouseClick();
+  sleep(1000);
+  robot.mouseClick();
+  sleep(1000);
   console.log('done');
+  console.log('hi matt');
 }
 
 function sleep(ms) {
