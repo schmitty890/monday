2775b3e9e2070d01d087d7be5caa48985eb0515e
Jason Schmitt <schmitty890@gmail.com>
2025-10-04 00:21:34 -0400

    coordinates

--- index.js
+++ index.js
@@ -4,11 +4,11 @@ function main() {
   console.log('starting');
   sleep(2000);
 
-  robot.moveMouseSmooth(331, 707);
+  robot.moveMouseSmooth(331, 743);
   robot.mouseClick();
   sleep(1000);
 
-  robot.moveMouseSmooth(355, 732);
+  robot.moveMouseSmooth(349, 769);
   robot.mouseClick();
   console.log('done');
   console.log('hi matt');
